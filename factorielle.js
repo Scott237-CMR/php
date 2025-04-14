9a3119b8fd65a368373e0aadc56a689325be7c2b
Scott237-CMR <tigerleo237@gmail.com>
2025-04-14 10:42:02 +0200

    Add files via upload

---
+++ factorielle.js
@@ -0,0 +1,11 @@
+let givenNumber = prompt("enter a number ");
+function givenNumber(number){
+    let factorial = 1;
+    for ( let i=1; i<=number; i++){
+        factorial=factorial*i;
+    }
+    return factorial;
+}
+//var myNumber=givenNumber(4);
+console.log('the factorial is:'+""+myNumber);
+alert('le factorial est :'+ factorial);
